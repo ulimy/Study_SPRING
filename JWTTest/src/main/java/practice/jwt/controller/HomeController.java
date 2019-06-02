dbaefd518ccaa8f7be0a25c307e085c1cf4163d7
= <ulim130@naver.com>
2019-06-02 17:46:47 +0900

    setting

---
+++ JWTTest/src/main/java/practice/jwt/controller/HomeController.java
@@ -0,0 +1,18 @@
+package practice.jwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public void main() {
+		return;
+	}
+	
+}
